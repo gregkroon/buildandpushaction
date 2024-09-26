b4885762f5e64f20eed2b9901dce4345b25101d6
gregkroon <44827446+gregkroon@users.noreply.github.com>
2024-09-26 10:38:16 +1000

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,19 +8,19 @@ IMAGE_NAME="${INPUT_IMAGE_NAME}"
 TAGS="${INPUT_TAGS:-latest}"  # Default to "latest" tag if none provided
 
 # Log in to Docker Hub
-echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin
+echo "$PASSWORD" | /usr/local/bin/docker login -u "$USERNAME" --password-stdin
 
 # Build and tag the Docker image for each specified tag
 for TAG in $(echo "$TAGS" | tr ',' '\n'); do
   echo "Building Docker image ${IMAGE_NAME}:${TAG}"
-  docker build -t "${IMAGE_NAME}:${TAG}" .
+  /usr/local/bin/docker build -t "${IMAGE_NAME}:${TAG}" .
 done
 
 # Push the Docker image for each tag
 for TAG in $(echo "$TAGS" | tr ',' '\n'); do
   echo "Pushing Docker image ${IMAGE_NAME}:${TAG}"
-  docker push "${IMAGE_NAME}:${TAG}"
+  /usr/local/bin/docker push "${IMAGE_NAME}:${TAG}"
 done
 
 # Log out from Docker Hub
-docker logout
+/usr/local/bin/docker logout
